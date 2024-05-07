20357073491f071bc5fbe91f3900ae503b9f00be
Katerina Sokolova Havlova <havlova.katarina@gmail.com>
2024-05-07 16:35:06 +0200

    mezistav

--- js/mobile-nav.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger-menu");
-const navsub = document.querySelector(".nav-sub");
-hamburger.addEventListener('click', () => {
-hamburger.classList.toggle("change")
-navsub.classList.toggle("nav-change")
-});
